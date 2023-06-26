9bda56e053587375cdb755597b39790a97642a64
Robson Melo de Souza <sys.robson@gmail.com>
2023-06-26 20:20:58 -0300

    changed styles and design of the blog

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Post from "../components/Post";
+import "./index.scss";
+import { Link } from "react-router-dom";
+import { format } from "date-fns";
 
 const Index = () => {
   const [posts, setPosts] = useState([]);
@@ -11,7 +14,49 @@ const Index = () => {
     });
   }, []);
   return (
-    <div>{posts.length > 0 && posts.map((post) => <Post {...post} />)}</div>
+    <div className="IndexPage">
+      <div className="IndexPage__Hero">
+        {posts.length > 0 && (
+          // <Post className="IndexPage__MainPost" {...posts[0]} />
+          <div className="MainPost">
+            <Link to={`/post/${posts[0]._id}`}>
+              <img
+                className="MainPost__Image"
+                src={"http://localhost:4000/" + posts[0].cover}
+                alt="Post image"
+              />
+            </Link>
+            <div className="MainPost__Summary">
+              <Link to={`/post/${posts[0]._id}`}>
+                <h2 className="MainPost__Title">{posts[0].title}</h2>
+              </Link>
+              <p className="MainPost__Info">
+                <a href="#" className="MainPost__Author">
+                  {posts[0].author.username}
+                </a>
+                <time className="MainPost__Date">
+                  {format(new Date(posts[0].createdAt), "dd/MM/yyyy HH:mm")}
+                </time>
+              </p>
+              <p className="MainPost__Description">{posts[0].summary}</p>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="MainBanner">
+        <button className="MainBanner__Button">Breaking News</button>
+        <p className="MainBanner__Text">
+          Kanye West says he's running for president in 2024
+        </p>
+      </div>
+      <div className="IndexPage__Posts">
+        {posts.length > 0 && posts.map((post) => <Post {...post} />)}
+      </div>
+
+      <div className="ViewMore">
+        <button className="ViewMore__Button">View More</button>
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -25,40 +25,47 @@ const PostPage = () => {
 
   return (
     <div className="PostPage">
-      <h1 className="PostPage__Title">{postInfo.title}</h1>
-      <time className="PostPage__Date">
-        {format(new Date(postInfo.createdAt), "dd/MM/yyyy HH:mm")}
-      </time>
-      <div className="PostPage__Author">By: @{postInfo.author.username}</div>
-      {userInfo?.id === postInfo.author._id && (
-        <div className="PostPage__Edit">
-          <Link to={`/edit/${postInfo._id}`} className="PostPage__Edit-btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-              />
-            </svg>
-            Edit this post
-          </Link>
+      <div className="PostPage__Header"></div>
+      <div className="PostPage__Container">
+        <h1 className="PostPage__Title">{postInfo.title}</h1>
+        <div className="PostPage__Image">
+          <img
+            src={`http://localhost:4000/${postInfo.cover}`}
+            alt={postInfo.title}
+          />
         </div>
-      )}
-      <div className="PostPage__Image">
-        <img
-          src={`http://localhost:4000/${postInfo.cover}`}
-          alt={postInfo.title}
-        />
-      </div>
+        <div className="PostPage__Info">
+          <time className="PostPage__Date">
+            {format(new Date(postInfo.createdAt), "dd/MM/yyyy HH:mm")}
+          </time>
+          <div className="PostPage__Author">
+            By: @{postInfo.author.username}
+          </div>
+          {userInfo?.id === postInfo.author._id && (
+            <div className="PostPage__Edit">
+              <Link to={`/edit/${postInfo._id}`} className="PostPage__Edit-btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
+                </svg>
+                Edit this post
+              </Link>
+            </div>
+          )}
+        </div>
 
-      <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+        <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+      </div>
     </div>
   );
 };
